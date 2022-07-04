e6c991c8a0522f515434f6a511fb912e022aeefa
Arnaldo Fagundes <arnaldo.s.fagundes@gmail.com>
2022-07-04 18:05:10 -0300

    Create new queries

--- src/main/java/br/com/certificatevalid/repository/CompanyRepository.java
+++ src/main/java/br/com/certificatevalid/repository/CompanyRepository.java
@@ -1,12 +1,18 @@
 package br.com.certificatevalid.repository;
 
 import br.com.certificatevalid.model.Company;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 
-    Boolean existsByCompanyVerificationCode(String companyVerificationCode);
+    Boolean existsByContactEmail(String contactEmail);
+
+    @Query("SELECT c FROM Company c WHERE LOWER(c.companyName) LIKE :companyName%")
+    Page<Company> findByCompanyName(String companyName, Pageable pageRequest);
 
 }
