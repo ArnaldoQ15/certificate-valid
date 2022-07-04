2ea7f63fb5cac71521a24f3aadc5b050ac0232c1
Arnaldo Fagundes <arnaldo.s.fagundes@gmail.com>
2022-07-04 18:02:59 -0300

    Update CertificateValidApplication.java

--- src/main/java/br/com/certificatevalid/CertificateValidApplication.java
+++ src/main/java/br/com/certificatevalid/CertificateValidApplication.java
@@ -2,8 +2,9 @@ package br.com.certificatevalid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class CertificateValidApplication {
 
     public static void main(String[] args) {
