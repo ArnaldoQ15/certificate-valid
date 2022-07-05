985eedb747231c8770a373cca9757b6f8c3e41da
Arnaldo Fagundes <arnaldo.s.fagundes@gmail.com>
2022-07-05 11:37:07 -0300

    Add new query

--- src/main/java/br/com/certificatevalid/repository/UserRepository.java
+++ src/main/java/br/com/certificatevalid/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package br.com.certificatevalid.repository;
 
+import br.com.certificatevalid.model.Company;
 import br.com.certificatevalid.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,5 +14,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByDocumentCpf (String cpf);
 
     Boolean existsByEmail (String email);
+
+    @Query("SELECT u FROM User u WHERE LOWER(u.username) LIKE :username%")
+    Page<User> findByUsername(String username, Pageable pageRequest);
 
 }
